906e408c76d2c71579f88c313a0187bbb4848c59
bosgood <bosgood@gmail.com>
2013-10-15 17:55:56 -0500

    Removing client from project

--- client/platforms/android/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-/Users/bosgood/dev/src/proj/voting/platforms/android/gen/com/bosgood/voting/cordova/R.java \
- : /Users/bosgood/dev/src/proj/voting/platforms/android/res/drawable/icon.png \
-/Users/bosgood/dev/src/proj/voting/platforms/android/res/drawable-hdpi/icon.png \
-/Users/bosgood/dev/src/proj/voting/platforms/android/res/drawable-ldpi/icon.png \
-/Users/bosgood/dev/src/proj/voting/platforms/android/res/drawable-mdpi/icon.png \
-/Users/bosgood/dev/src/proj/voting/platforms/android/res/drawable-xhdpi/icon.png \
-/Users/bosgood/dev/src/proj/voting/platforms/android/res/values/strings.xml \
-/Users/bosgood/dev/src/proj/voting/platforms/android/res/xml/config.xml \
-/Users/bosgood/dev/src/proj/voting/platforms/android/bin/AndroidManifest.xml \
--- client/webapp/app/scripts/router.js
+++
@@ -1,3 +0,0 @@
-Yeoman.Router.map(function () {
-  
-});
